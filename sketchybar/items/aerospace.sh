bbb6ce181c666f1f78475f8b33b7eafe96f87b6b
Daniel Macuare <daniel280187@hotmail.com>
2025-01-29 16:02:32 +0100

    Sketchybar - Initial commit

---
+++ sketchybar/items/aerospace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ../colors.sh
+sketchybar --add event aerospace_workspace_change
+
+for sid in $(aerospace list-workspaces --all); do
+    sketchybar --add item space.$sid left \
+        --subscribe space.$sid aerospace_workspace_change \
+        --set space.$sid \
+        background.color=$ACCENT_COLOR \
+        background.corner_radius=5 \
+        background.height=20 \
+        label="$sid" \
+        label.padding_right=20 \
+        label.y_offset=-1 \
+        click_script="aerospace workspace $sid" \
+        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+done
