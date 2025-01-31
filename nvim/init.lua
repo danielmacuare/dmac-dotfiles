71b60d8cf0c55c7b47f02fbd4a709f5382815b9d
Daniel Macuare <daniel280187@hotmail.com>
2025-01-31 10:31:38 +0100

    nvim: Adding autoformatting plugin none-ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,29 +1,29 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
 -- Install package manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-}
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 -- Setup plugins
-require('lazy').setup({
-  require 'plugins.catpuccin',
-  require 'plugins.neo-tree',
-  require 'plugins.bufferline',
-  require 'plugins.lualine',
-  require 'plugins.treesitter',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.autocompletion',
+require("lazy").setup({
+    require("plugins.catpuccin"),
+    require("plugins.neo-tree"),
+    require("plugins.bufferline"),
+    require("plugins.lualine"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lsp"),
+    require("plugins.autocompletion"),
+    require("plugins.autoformatting"),
 })
