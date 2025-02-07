8245a40c3b96ecc3e35ae17533a54c06c4a82613
Daniel Macuare <daniel280187@hotmail.com>
2025-02-07 13:37:28 +0100

    wezterm: Adding paste with right click

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@
 local wezterm = require("wezterm")
 
 local k = require("utils/keys")
+local act = wezterm.action
 
 ---@type Config
 ---@diagnostic disable: missing-fields
@@ -19,5 +20,21 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 config.send_composed_key_when_left_alt_is_pressed = true
 config.term = "wezterm"
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
 
 return config
