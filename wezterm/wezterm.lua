3400b896b74237b3649f60ec1be9b257b0089c39
Daniel Macuare <daniel280187@hotmail.com>
2025-02-09 00:09:35 +0100

    wezterm: Switching to Dracula Theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- TARGET=~/.config/wezterm/wezterm.lua
--- Pull in the Wezterm
+-- Wezterm Config File
 local wezterm = require("wezterm")
 
 local k = require("utils/keys")
@@ -15,7 +15,7 @@ config.font = wezterm.font("MesloLGSDZ Nerd Font Mono")
 config.font_size = 19
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Dracula+"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 config.send_composed_key_when_left_alt_is_pressed = true
@@ -36,5 +36,4 @@ config.mouse_bindings = {
 		end),
 	},
 }
-
 return config
