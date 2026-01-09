6bb320c67392fd9bdfddcbc0c08e5ecbf12e3f62
Daniel Macuare <daniel280187@hotmail.com>
2026-01-09 15:43:05 +0100

    nvim: 0-based index for columns

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -103,7 +103,14 @@ return {
         lualine_b = { 'branch' },
         lualine_c = { filename },
         lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
-        lualine_y = { 'location' },
+        -- 0-based index for colums
+        lualine_y = {
+          function()
+            local line = vim.fn.line '.'
+            local col = vim.fn.col '.' - 1
+            return string.format('%d:%d', line, col)
+          end,
+        },
         lualine_z = { 'progress' },
       },
       inactive_sections = {
